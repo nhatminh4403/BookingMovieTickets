386507a296bbd8f1036dffd7f0ec0d0919898389
nhatminh4403 <145756375+nhatminh4403@users.noreply.github.com>
2024-04-18 22:12:32 +0700

    Model

--- BookingMovieTickets/Program.cs
+++ BookingMovieTickets/Program.cs
@@ -1,7 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using MoviesBooking.DataAccess;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<MoviesBookingDBContext>(
+    options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
+
 
 var app = builder.Build();
 
