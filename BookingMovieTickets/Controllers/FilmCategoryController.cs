8fa8c2915d78055a35d778112bfb1fad33562f3b
Bui Chi Bao <buichibao1601@gmail.com>
2024-04-25 23:01:23 +0700

    tao controlller loai film

---
+++ BookingMovieTickets/Controllers/FilmCategoryController.cs
@@ -0,0 +1,95 @@
+﻿using BookingMovieTickets.Repository.Interface;
+using Microsoft.AspNetCore.Mvc;
+using MoviesBooking.Models;
+
+namespace BookingMovieTickets.Controllers
+{
+    public class FilmCategoryController : Controller
+    {
+        private readonly I_FilmRepository _FilmRepository;
+        private readonly I_FilmCategoryRepository _FilmCategoryRepository;
+        public FilmCategoryController(I_FilmRepository FilmRepository, I_FilmCategoryRepository FilmCategoryRepository)
+
+        {
+            _FilmRepository = FilmRepository;
+            _FilmCategoryRepository = FilmCategoryRepository;
+        }
+        public async Task<IActionResult> Index()
+        {
+
+            var categories = await _FilmCategoryRepository.GetAllAsync();
+            ViewBag.categories = categories.ToList();
+            return View(categories);
+        }
+
+        public async Task<IActionResult> Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(FilmCategory filmCategory)
+        {
+            if (ModelState.IsValid)
+            {
+                await _FilmCategoryRepository.AddAsync(filmCategory);
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(filmCategory);
+        }
+
+
+        public async Task<IActionResult> Edit(int id)
+        {
+
+            var FilmCategory = await _FilmCategoryRepository.GetByIdAsync(id);
+            if (FilmCategory == null)
+            {
+                return NotFound();
+            }
+            ViewBag.FilmCategory = FilmCategory;
+            return View(FilmCategory);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, FilmCategory FilmCategory)
+        {
+            if (id != FilmCategory.FilmCategoryId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                var existingCategory = await _FilmCategoryRepository.GetByIdAsync(id);
+
+                existingCategory.Name = FilmCategory.Name;
+
+                await _FilmCategoryRepository.UpdateAsync(existingCategory);
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(FilmCategory);
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+
+            var FilmCategory = await _FilmCategoryRepository.GetByIdAsync(id);
+            if (FilmCategory == null)
+            {
+                return NotFound();
+            }
+            ViewBag.FilmCategory = FilmCategory;
+            return View(FilmCategory);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            await _FilmCategoryRepository.DeleteAsync(id);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
