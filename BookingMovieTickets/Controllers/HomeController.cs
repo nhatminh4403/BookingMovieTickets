6afaa732222aac0ad91129013f750cdf34f9f0bd
nhatminh4403 <145756375+nhatminh4403@users.noreply.github.com>
2024-05-07 15:00:48 +0700

    view details

--- BookingMovieTickets/Controllers/HomeController.cs
+++ BookingMovieTickets/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
 using BookingMovieTickets.Models;
+using BookingMovieTickets.Repository.Interface;
+using BookingMovieTickets.VIewModel;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using MoviesBooking.DataAccess;
 using MoviesBooking.Models;
 using System.Diagnostics;
 
@@ -10,14 +13,50 @@ namespace BookingMovieTickets.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly UserManager<UserInfo> _userManager;
-        public HomeController(ILogger<HomeController> logger, UserManager<UserInfo> userManager)
+        private readonly I_FilmCategoryRepository _filmCategoryRepository;
+        private readonly I_FilmRepository _filmRepository;
+        private readonly I_Seat _seatRepo;
+        private readonly BookingMovieTicketsDBContext _dbContext;
+        private readonly I_PremiereTime _premiereTimeRepo;
+        private readonly I_Schedule _scheduleRepo;
+        private readonly I_TheatreRoom _theatreRoomRepo;
+        private readonly I_Theater _TheaterRepo;
+
+        public HomeController(ILogger<HomeController> logger, UserManager<UserInfo> userManager, I_FilmCategoryRepository filmCategoryRepository, I_FilmRepository filmRepository,
+            I_PremiereTime premiereTime, I_Seat seatRepo, I_Schedule scheduleRepo, I_TheatreRoom theatreRoomRepo, I_Theater theaterRepo, BookingMovieTicketsDBContext dbContext)
         {
             _logger = logger;
             _userManager = userManager;
+            _filmRepository = filmRepository;
+            _seatRepo = seatRepo;
+            _scheduleRepo = scheduleRepo;
+            _filmCategoryRepository = filmCategoryRepository;
+            _premiereTimeRepo = premiereTime;
+            _theatreRoomRepo = theatreRoomRepo;
+            _TheaterRepo = theaterRepo;
+            _dbContext = dbContext;
         }
 
         public async Task<IActionResult> Index()
         {
+            var films = await _filmRepository.GetAllAsync();
+            var categories = await _filmCategoryRepository.GetAllAsync();
+            var seats = await _seatRepo.GetAllSeatAsync();
+            var schedules = await _scheduleRepo.GetAllAsync();
+            var premiere = await _premiereTimeRepo.GetAllAsync();
+            var rooms = await _theatreRoomRepo.GetAllRoomAsync();
+            var theaters = await _TheaterRepo.GetAllAsync();
+            var filmVM = new FilmVM
+            {
+                Films = films,
+                FilmCategories = categories,
+                Seats = seats,
+                FilmSchedules = schedules,
+                PremiereTime = premiere,
+                TheatreRooms = rooms,
+                Theatres = theaters
+            };
+          
             if (User.Identity.IsAuthenticated)
             {
                 var user = await _userManager.GetUserAsync(User);
@@ -29,10 +68,13 @@ namespace BookingMovieTickets.Controllers
                     }
                 }
                
-            }
-            return View();
+            }  return View(filmVM);
         }
-
+        public async Task<IActionResult> FilmDetailView(int id)
+        {
+            var productsDetail = await _filmRepository.GetByIdAsync(id);
+            return View(productsDetail);
+        }
         public IActionResult Privacy()
         {
             return View();
