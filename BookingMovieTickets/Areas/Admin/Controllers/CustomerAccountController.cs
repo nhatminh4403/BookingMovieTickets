25eadcb8074da5c753d0049a0f3cb69823bbd1ac
nhatminh4403 <145756375+nhatminh4403@users.noreply.github.com>
2024-05-18 23:48:10 +0700

    login ở Home, quản lý tài khoản controller ở Admin

---
+++ BookingMovieTickets/Areas/Admin/Controllers/CustomerAccountController.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MoviesBooking.Models;
+using System.Data;
+
+namespace BookingMovieTickets.Areas.Admin.Controllers
+{
+    [Authorize(Roles = UserRole.Role_Admin)]
+    [Area("Admin")]
+    public class CustomerAccountController : Controller
+    {
+        private readonly UserManager<UserInfo> _userManager;
+        public CustomerAccountController(UserManager<UserInfo> userManager)
+        {
+            _userManager = userManager;
+        }
+        public async Task<IActionResult> Index()
+        {
+            IList<UserInfo> customer = await _userManager.GetUsersInRoleAsync("Customer");
+            return View(customer);
+        }
+
+        // GET: CustomerAccountController/Details/5
+        public  ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: CustomerAccountController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CustomerAccountController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CustomerAccountController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: CustomerAccountController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CustomerAccountController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CustomerAccountController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
